035a25c9ebd830994e5cd5d59f7d192be5c2234e
Blayneraptor <cousoworks@gmail.com>
2025-02-25 21:27:44 +0100

    Cuadrando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,15 @@ import './Footer.css';
 import logo from '../assets/logo.png';  // Importa el logo desde la carpeta assets
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear(); // Obtiene el año actual
+
   return (
     <footer className="footer-container">
-     
       <div className="footer-info">
-         <div className="footer-logo">
-        <img src={logo} alt="Logo Móvil Center" className="footer-logo-image" />
-      </div>
+        <div className="footer-logo">
+          <img src={logo} alt="Logo Móvil Center" className="footer-logo-image" />
+        </div>
         <div className="footer-contact">
-            
           <h4>Datos de contacto</h4>
           <p><strong>Dirección:</strong> C/ Avenida Loveless, Madrid</p>
           <p><strong>Teléfono de contacto:</strong> 999 999 999</p>
@@ -19,16 +19,15 @@ const Footer = () => {
         <div className="footer-links">
           <h4>Enlaces de interés</h4>
           <ul>
-            <li><a href="/envios">Envíos</a></li>
-            <li><a href="/metodos-pago">Métodos de pago</a></li>
-            <li><a href="/devoluciones">Devoluciones</a></li>
-            <li><a href="/plazos">Plazos</a></li>
+            <li><a href="/">Envíos</a></li>
+            <li><a href="/">Métodos de pago</a></li>
+            <li><a href="/">Devoluciones</a></li>
+            <li><a href="/">Plazos</a></li>
           </ul>
         </div>
       </div>
       <div className="footer-footer">
-        <p>© 2025 Blayne Shop | Diseñado por Blayneraptor</p>
-        <p><a href="/aviso-legal">Aviso Legal</a> | <a href="/politica-privacidad">Política de privacidad</a> | <a href="/politica-cookies">Política de Cookies</a></p>
+        <p>© {currentYear} Blayne Shop | Diseñado por Blayneraptor</p>
       </div>
     </footer>
   );
